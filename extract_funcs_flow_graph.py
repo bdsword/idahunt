8af2ad54e89fcc0dca71926948c5d3492966d3f8
BDSword <thetruebloodsword@gmail.com>
2018-01-21 17:46:22 +0800

    Add script to extract flow graph for each function in the binary.

---
+++ extract_funcs_flow_graph.py
@@ -0,0 +1,23 @@
+from idautils import *
+from idaapi import *
+from idc import *
+import os
+
+
+path = GetIdbPath().rsplit('.')[0]
+
+func_dir_path = path + '_functions'
+if not os.path.isdir(func_dir_path):
+    os.mkdir(func_dir_path)
+
+f = open(os.path.join(func_dir_path, 'functions_list.txt'), 'w')
+
+for segea in Segments():
+    for funcea in Functions(segea, SegEnd(segea)):
+        function_name = GetFunctionName(funcea)
+        end_ea = FindFuncEnd(funcea)
+        GenFuncGdl(os.path.join(func_dir_path, function_name), "Call Gdl", funcea, end_ea, CHART_GEN_GDL | CHART_PRINT_NAMES);
+        f.write(function_name + '\n')
+f.close()
+Exit(0)
+
