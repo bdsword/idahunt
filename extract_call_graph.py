1900f0476bae22e82b28a38a37fccd0da0ae8c63
bdsword <thetruebloodsword@gmail.com>
2018-02-27 19:20:54 +0800

    Avoid re-generate .gdl file.

--- extract_call_graph.py
+++ extract_call_graph.py
@@ -5,6 +5,9 @@ import os
 cur = idc.MinEA()
 end = idc.MaxEA()
 path = os.path.splitext(idc.GetIdbPath())[0] + '.gdl'
+if os.path.isfile(path):
+    idc.Message('Gdl file is existed for {}'.format(path))
+    idc.Exit(-1)
 idc.GenCallGdl(path, None, idc.CHART_GEN_GDL)
 idc.Message('Gdl file has been saved to {}\n'.format(path))
 idc.Exit(0)
