352048bce27ffd569aff5125ca2ac114593a5dac
bdsword <thetruebloodsword@gmail.com>
2018-08-16 17:51:51 +0800

    Enable extract dot format call graph directly with constant option.

--- extract_call_graph.py
+++ extract_call_graph.py
@@ -1,11 +1,14 @@
 import idc
 import idaapi
 import os
+import datetime
 
 cur = idc.MinEA()
 end = idc.MaxEA()
-path = os.path.splitext(idc.GetIdbPath())[0] + '.gdl'
-idc.GenCallGdl(path, None, idc.CHART_GEN_GDL)
+# path = os.path.splitext(idc.GetIdbPath())[0] + '.gdl'
+# idc.GenCallGdl(path, None, idc.CHART_GEN_GDL)
+path = os.path.splitext(idc.GetIdbPath())[0] + '.dot'
+idc.GenCallGdl(path, None, 0x2000) # 0x2000 -> CHART_GEN_DOT
 idc.Message('Gdl file has been saved to {}\n'.format(path))
 idc.Exit(0)
 
